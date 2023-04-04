e5d1ca6171dcbeb0bcde36144a4e9ea2d2d8631f
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-04-04 22:20:13 +0200

    Restructure in favor of an aggregator

--- src/lib.rs
+++ src/lib.rs
@@ -11,32 +11,37 @@
 //! # Usage
 //!
 //! ```rust
-//! use dusk_merkle::{MerkleTree, ToMerkleInputs, MerkleHash};
+//! use dusk_merkle::{MerkleTree, MerkleAggregator};
 //!
-//! struct TestHash;
-//! impl MerkleHash for TestHash {
-//!     type Hash = u8;
+//! struct TestAggregator;
+//! impl MerkleAggregator for TestAggregator {
+//!     type Item = u8;
 //!
-//!     fn merkle_hash<I, const N: usize>(inputs: I) -> Self::Hash
-//!     where
-//!         I: ToMerkleInputs<Self::Hash, N>,
+//!     fn merkle_zero(_height: u32) -> Self::Item {
+//!         0
+//!     }
+//!
+//!     fn merkle_hash<'a, I>(items: I) -> Self::Item
+//!         where
+//!             Self::Item: 'a,
+//!             I: IntoIterator<Item = &'a Self::Item>,
 //!     {
-//!         // Adding numbers is not a cryptographic hash
-//!         let inputs = inputs.to_merkle_inputs();
-//!         inputs.into_iter().fold(0, u8::wrapping_add)
+//!         items
+//!             .into_iter()
+//!             .fold(0, |acc, x| u8::wrapping_add(acc, *x))
 //!     }
 //! }
 //!
 //! const HEIGHT: u32 = 3;
 //! const ARITY: usize = 2;
 //!
-//! let mut tree = MerkleTree::<TestHash, HEIGHT, ARITY>::new();
+//! let mut tree = MerkleTree::<TestAggregator, HEIGHT, ARITY>::new();
 //!
 //! // No elements have been inserted so the root is `None`.
 //! assert!(matches!(tree.root(), None));
 //!
-//! tree.insert(4, &21u8);
-//! tree.insert(7, &21u8);
+//! tree.insert(4, [&21u8]);
+//! tree.insert(7, [&21u8]);
 //!
 //! println!("{:?}", tree.root());
 //! // After elements have been inserted, there will be a root.
@@ -54,45 +59,35 @@
 extern crate alloc;
 use alloc::boxed::Box;
 
-/// A type that can be decomposed into multiple inputs to a hash.
-pub trait ToMerkleInputs<T, const N: usize> {
-    /// Returns the hash inputs associated with the type.
-    fn to_merkle_inputs(&self) -> [T; N];
-}
+/// A reducing function that takes a collection of items of a given type and
+/// returns one item of the same type.
+pub trait MerkleAggregator {
+    /// The item processed by the aggregator.
+    type Item;
 
-impl<T: Clone, const N: usize> ToMerkleInputs<T, N> for [T; N] {
-    fn to_merkle_inputs(&self) -> [T; N] {
-        self.clone()
-    }
-}
+    /// Returns the zero value to be used for a hash. This value can depend on
+    /// the `height` it is necessary at.
+    fn merkle_zero(height: u32) -> Self::Item;
 
-impl<T: Clone> ToMerkleInputs<T, 1> for T {
-    fn to_merkle_inputs(&self) -> [T; 1] {
-        [self.clone()]
-    }
-}
-
-pub trait MerkleHash {
-    type Hash;
-
-    fn merkle_hash<I, const N: usize>(inputs: I) -> Self::Hash
+    /// Aggregates the given `items`.
+    fn merkle_hash<'a, I>(items: I) -> Self::Item
     where
-        I: ToMerkleInputs<Self::Hash, N>;
+        Self::Item: 'a,
+        I: IntoIterator<Item = &'a Self::Item>;
 }
 
 #[derive(Debug, Clone)]
-struct Node<H: MerkleHash, const ARITY: usize> {
+struct Node<A: MerkleAggregator, const ARITY: usize> {
     num_leaves: u64,
-    hash: Option<H::Hash>,
-    children: [Option<Box<Node<H, ARITY>>>; ARITY],
+    hash: Option<A::Item>,
+    children: [Option<Box<Node<A, ARITY>>>; ARITY],
 }
 
-impl<H, const ARITY: usize> Node<H, ARITY>
+impl<A, const ARITY: usize> Node<A, ARITY>
 where
-    H: MerkleHash,
-    <H as MerkleHash>::Hash: Default + Copy,
+    A: MerkleAggregator,
 {
-    const INIT: Option<Box<Node<H, ARITY>>> = None;
+    const INIT: Option<Box<Node<A, ARITY>>> = None;
 
     const fn new() -> Self {
         Self {
@@ -102,17 +97,13 @@ where
         }
     }
 
-    fn insert<T, const N: usize>(
-        &mut self,
-        height: u32,
-        position: u64,
-        element: &T,
-    ) where
-        T: ToMerkleInputs<H::Hash, N>,
+    fn insert<'a, I>(&mut self, height: u32, position: u64, items: I)
+    where
+        A::Item: 'a,
+        I: IntoIterator<Item = &'a A::Item>,
     {
         if height == 1 {
-            let inputs = element.to_merkle_inputs();
-            self.hash = Some(H::merkle_hash(inputs));
+            self.hash = Some(A::merkle_hash(items));
             self.num_leaves += 1;
             return;
         }
@@ -132,16 +123,15 @@ where
 
         // We just inserted a child at the given index.
         let child = self.children[child_index].as_mut().unwrap();
-        Self::insert(child, height - 1, child_pos, element);
+        Self::insert(child, height - 1, child_pos, items);
 
-        let mut hashes = [H::Hash::default(); ARITY];
-        for (i, child) in self.children.iter().enumerate() {
-            if let Some(child) = child {
-                hashes[i] = child.hash.unwrap();
-            }
-        }
+        let merkle_zero = A::merkle_zero(height);
+        let hash = A::merkle_hash(self.children.iter().map(|c| match c {
+            None => &merkle_zero,
+            Some(child) => child.hash.as_ref().unwrap(),
+        }));
 
-        self.hash = Some(H::merkle_hash(hashes));
+        self.hash = Some(hash);
         self.num_leaves += 1;
     }
 }
@@ -151,16 +141,18 @@ const fn capacity(arity: u64, height: u32) -> u64 {
 }
 
 /// A sparse Merkle tree.
-pub struct MerkleTree<H: MerkleHash, const HEIGHT: u32, const ARITY: usize> {
-    root: Node<H, ARITY>,
+pub struct MerkleTree<
+    A: MerkleAggregator,
+    const HEIGHT: u32,
+    const ARITY: usize,
+> {
+    root: Node<A, ARITY>,
 }
 
-impl<H, const HEIGHT: u32, const ARITY: usize> MerkleTree<H, HEIGHT, ARITY>
-where
-    H: MerkleHash,
-    <H as MerkleHash>::Hash: Default + Copy,
+impl<A: MerkleAggregator, const HEIGHT: u32, const ARITY: usize>
+    MerkleTree<A, HEIGHT, ARITY>
 {
-    /// Create a new merkle tree
+    /// Create a new merkle tree.
     #[must_use]
     pub const fn new() -> Self {
         Self { root: Node::new() }
@@ -170,15 +162,16 @@ where
     ///
     /// # Panics
     /// If `position >= capacity`.
-    pub fn insert<T, const N: usize>(&mut self, position: u64, element: &T)
+    pub fn insert<'a, I>(&mut self, position: u64, items: I)
     where
-        T: ToMerkleInputs<H::Hash, N>,
+        A::Item: 'a,
+        I: IntoIterator<Item = &'a A::Item>,
     {
-        self.root.insert(HEIGHT, position, element);
+        self.root.insert(HEIGHT, position, items);
     }
 
     /// Get the root of the merkle tree.
-    pub fn root(&self) -> Option<&H::Hash> {
+    pub fn root(&self) -> Option<&A::Item> {
         self.root.hash.as_ref()
     }
 
@@ -205,22 +198,28 @@ where
 mod tests {
     use super::*;
 
-    struct TestHash;
-    impl MerkleHash for TestHash {
-        type Hash = u8;
+    struct TestAggregator;
+    impl MerkleAggregator for TestAggregator {
+        type Item = u8;
 
-        fn merkle_hash<I, const N: usize>(inputs: I) -> Self::Hash
+        fn merkle_zero(_height: u32) -> Self::Item {
+            0
+        }
+
+        fn merkle_hash<'a, I>(items: I) -> Self::Item
         where
-            I: ToMerkleInputs<Self::Hash, N>,
+            Self::Item: 'a,
+            I: IntoIterator<Item = &'a Self::Item>,
         {
-            let inputs = inputs.to_merkle_inputs();
-            inputs.into_iter().fold(0, u8::wrapping_add)
+            items
+                .into_iter()
+                .fold(0, |acc, x| u8::wrapping_add(acc, *x))
         }
     }
 
     #[test]
     fn new_node() {
-        let node = Node::<TestHash, 4>::new();
+        let node = Node::<TestAggregator, 4>::new();
 
         for child in &node.children {
             assert!(child.is_none(), "All children should start as `None`");
@@ -234,10 +233,12 @@ mod tests {
         const HEIGHT: u32 = 3;
         const ARITY: usize = 2;
 
-        let mut tree = MerkleTree::<TestHash, HEIGHT, ARITY>::new();
+        let mut tree = MerkleTree::<TestAggregator, HEIGHT, ARITY>::new();
 
-        tree.insert(5, &42u8);
-        tree.insert(5, &42u8);
+        tree.insert(5, [&42u8]);
+        tree.insert(5, [&42u8]);
+
+        assert_eq!(tree.len(), 2, "Two items were inserted");
     }
 
     #[test]
@@ -246,8 +247,8 @@ mod tests {
         const HEIGHT: u32 = 3;
         const ARITY: usize = 2;
 
-        let mut tree = MerkleTree::<TestHash, HEIGHT, ARITY>::new();
+        let mut tree = MerkleTree::<TestAggregator, HEIGHT, ARITY>::new();
 
-        tree.insert(tree.capacity(), &42u8);
+        tree.insert(tree.capacity(), [&42u8]);
     }
 }
